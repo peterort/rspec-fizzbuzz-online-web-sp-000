83164fb83af434ec8677a832fe05b2061852e058
Peter Ort <peter@peterort.com>
2020-02-18 17:26:47 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,33 +3,16 @@
 
 def fizzbuzz(number)
   if number % 3 == 0 && number % 5 == 0
-  return "FizzBuzz"
+    return "FizzBuzz"
 
-elsif number % 3 == 0
+  elsif number % 3 == 0
     return "Fizz"
 
   elsif number % 5 == 0
     return "Buzz"
 
 
-
   else
-    return "False"
+    return "nil"
   end
 end
-
-
-#def fizzbuzz(number)
-#  if number % 3 == 0
-#    return "Fizz"
-#
-#  elsif number % 5 == 0
-#    return "Buzz"
-#
-#  elsif number % 3 == 0 && number % 5 == 0
-#    return "FizzBuzz"
-#
-#  else
-#    return "False"
-#  end
-#end
