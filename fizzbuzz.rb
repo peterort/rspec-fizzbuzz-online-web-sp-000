93ab9ac62884e13add145c68485565fcad01ddf3
Peter Ort <peter@peterort.com>
2020-02-18 19:38:38 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -8,12 +8,12 @@ def fizzbuzz(number)
   elsif number % 3 == 0
     return "Fizz"
 
-  else number % 5 == 0
+  elsif number % 5 == 0
     return "Buzz"
 
-
+  else number % 3 == 0 || number % 5 == 0
 
   end
 end
 
-number % 3 == 0 !|| number % 5 == 0
+
